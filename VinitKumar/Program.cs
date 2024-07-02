595f74e30e4dbe4d4248a5cf535dee3b81315b47
Vinit Kumar <kumarvini@microsoft.com>
2024-07-02 12:40:59 -0700

    Problem 1

---
+++ VinitKumar/Program.cs
@@ -0,0 +1,8 @@
+﻿// Problem 1
+
+using VinitKumar.Problems;
+using VinitKumar.Utilities;
+
+Problem1 problem1Instance = new();
+TreeNode? BinaryTreeRoot = problem1Instance.CreateABinaryTree();
+problem1Instance.InOrderTraversalRecursive(BinaryTreeRoot);
---
+++ VinitKumar/Utilities/TreeNode.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace VinitKumar.Utilities
+{
+    public class TreeNode(int Value)
+    {
+        public int Val { get; set; } = Value;
+        public TreeNode? Left { get; set; } = null;
+        public TreeNode? Right { get; set; } = null;
+    }
+}
