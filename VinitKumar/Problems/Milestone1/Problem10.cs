fcb39d83023d674373c00e090bc70bf61dd3f895
Vinit Kumar <kumarvini@microsoft.com>
2024-07-24 21:06:51 -0700

    Changes

--- VinitKumar/Problems/Milestone1/Problem10.cs
+++ VinitKumar/Problems/Milestone1/Problem10.cs
@@ -12,8 +12,15 @@ namespace VinitKumar.Problems.Milestone1
 {
 	public static class Problem10
 	{
-		public Class1()
-		{
-		}
+		public static void TestM()
+        {
+            IList<IList<int>> lst = new List<IList<int>>();
+			lst.Add(new List<int>());
+			lst[0].Add(1);
+            lst[0].Add(2);
+            lst[0].Add(3);
+
+        }		
+
 	}
 }
