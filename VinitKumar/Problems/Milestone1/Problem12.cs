790cdef32a4aeee1e79c2b295f3a3193d732c44b
Vinit Kumar <kumarvini@microsoft.com>
2024-07-28 22:12:02 -0700

    Problem 12 :: 700. Search in a Binary Search Tree

--- VinitKumar/Problems/Milestone1/Problem12.cs
+++ VinitKumar/Problems/Milestone1/Problem12.cs
@@ -8,12 +8,9 @@ namespace VinitKumar.Problems.Milestone1
         public static TreeNode? SearchBST(TreeNode? root, int val)
         {
             if (root == null) return null;
-
-            if (root.val == val) return root;
-
+            if (root?.val == val) return root;
             TreeNode? left = SearchBST(root?.left, val);
             TreeNode? right = SearchBST(root?.right, val);
-
             return left ?? right;
         }
     }
