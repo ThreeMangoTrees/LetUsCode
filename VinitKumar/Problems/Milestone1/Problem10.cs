6815da49236189bd4e41c0b81f18c8feb55cbb03
Vinit Kumar <kumarvini@microsoft.com>
2024-07-28 22:09:41 -0700

    Problem 10 :: 102. Binary Tree Level Order Traversal

--- VinitKumar/Problems/Milestone1/Problem10.cs
+++ VinitKumar/Problems/Milestone1/Problem10.cs
@@ -10,17 +10,67 @@ using VinitKumar.Utilities;
 
 namespace VinitKumar.Problems.Milestone1
 {
-	public static class Problem10
+	public class Problem10
 	{
-		public static void TestM()
+        IList<IList<int>>? ansR;
+        private IList<IList<int>> LevelOrderIterative(TreeNode? root)
         {
-            IList<IList<int>> lst = new List<IList<int>>();
-			lst.Add(new List<int>());
-			lst[0].Add(1);
-            lst[0].Add(2);
-            lst[0].Add(3);
+            IList<IList<int>> ans = new List<IList<int>>();
+            if (root == null)
+            {
+                return ans;
+            }
+            Queue<TreeNode> q = new Queue<TreeNode>();
+            q.Enqueue(root);
+            while (q.Count > 0)
+            {
+                int qsz = q.Count;
+                List<int> tlist = new List<int>();
+                while (qsz > 0)
+                {
+                    TreeNode crnt = q.Dequeue();
+                    tlist.Add(crnt.val);
 
-        }		
+                    if (crnt.left != null)
+                    {
+                        q.Enqueue(crnt.left);
+                    }
+                    if (crnt.right != null)
+                    {
+                        q.Enqueue(crnt.right);
+                    }
+                    qsz -= 1;
+                }
+                ans.Add(tlist);
+            }
+            return ans;
+        }
+        
+        private void LevelOrderRecursive(TreeNode? root, int level)
+        {
+            if (root == null)
+            {
+                return;
+            }
 
-	}
+            if (ansR?.Count == level)
+            {
+                ansR.Add(new List<int>());
+            }
+
+            ansR?[level].Add(root.val);
+
+            LevelOrderRecursive(root?.left, level + 1);
+            LevelOrderRecursive(root?.right, level + 1);
+        }
+        public IList<IList<int>> LevelOrder(TreeNode root)
+        {
+            // return LevelOrderIterative(root);
+
+            ansR = new List<IList<int>>();
+            LevelOrderRecursive(root, 0);
+            return ansR;
+        }
+
+    }
 }
