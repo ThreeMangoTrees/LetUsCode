51ac481045ddbe763ffa740027c9089161325a58
Vinit Kumar <kumarvini@microsoft.com>
2024-07-28 22:48:51 -0700

    Problem 13

--- VinitKumar/Problems/Milestone1/Problem13.cs
+++ VinitKumar/Problems/Milestone1/Problem13.cs
@@ -5,7 +5,7 @@ namespace VinitKumar.Problems.Milestone1
 {
     public class Problem13
     {
-        public static int FindSecondMinimumValue(TreeNode root)
+        private int TwoVariableApproach(TreeNode root)
         {
             if (root == null) return -1;
             Queue<TreeNode> q = new();
@@ -15,19 +15,17 @@ namespace VinitKumar.Problems.Milestone1
             while (q.Count > 0)
             {
                 TreeNode crnt = q.Dequeue();
-
                 if (crnt.val != mn)
                 {
                     if (result == 0)
                     {
                         result = crnt.val;
                     }
-                    else if (crnt.val < result)
+                    else
                     {
-                        result = crnt.val;
+                        result = Math.Min(result, crnt.val);
                     }
                 }
-
                 if (crnt.left != null)
                 {
                     q.Enqueue(crnt.left);
@@ -39,6 +37,38 @@ namespace VinitKumar.Problems.Milestone1
             }
 
             return result == 0 ? -1 : result;
+        }
+
+        private int SortedSetApproach(TreeNode root)
+        {
+            SortedSet<int> sset = new SortedSet<int>();
+            Queue<TreeNode> q = new Queue<TreeNode>();
+            q.Enqueue(root);
+
+            while (q.Count > 0)
+            {
+                TreeNode crnt = q.Dequeue();
+                sset.Add(crnt.val);
+                if (sset.Count > 2)
+                {
+                    sset.Remove(sset.Max);
+                }
+                if (crnt.left != null)
+                {
+                    q.Enqueue(crnt.left);
+                }
+                if (crnt.right != null)
+                {
+                    q.Enqueue(crnt.right);
+                }
+            }
+            return sset.Min == sset.Max ? -1 : sset.Max;
+        }
+
+        public int FindSecondMinimumValue(TreeNode root)
+        {
+            // return FindSecondMinimumValueQueueApproach(root);
+            return SortedSetApproach(root);
         }
 
     }
