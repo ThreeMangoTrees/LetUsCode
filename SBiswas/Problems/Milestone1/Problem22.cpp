8b0e47cea479c78edc2a1120b993cb06d2f901df
notu97 <shiladitya97@outlook.com>
2024-08-07 23:23:30 -0700

    Problem 23: Merge two binary trees
    Signed-off-by: notu97 <shiladitya97@outlook.com>

---
+++ SBiswas/Problems/Milestone1/Problem22.cpp
@@ -0,0 +1,44 @@
+#include<iostream>
+#include<vector>
+#include<unordered_map>
+
+using namespace std;
+
+
+//  Definition for a binary tree node.
+struct TreeNode {
+    int val;
+    TreeNode *left;
+    TreeNode *right;
+    TreeNode() : val(0), left(nullptr), right(nullptr) {}
+    TreeNode(int x) : val(x), left(nullptr), right(nullptr) {}
+    TreeNode(int x, TreeNode *left, TreeNode *right) : val(x), left(left), right(right) {}
+ };
+
+class Solution {
+public:
+    TreeNode* mergeTrees(TreeNode* root1, TreeNode* root2) {
+
+        return helper(root1, root2);
+    }
+
+    TreeNode* helper(TreeNode* root1, TreeNode* root2)
+    {
+        if(!root1)
+        {
+            return root2;
+        }
+
+        if(!root2)
+        {
+            return root1;
+        }
+
+        root1->val += root2->val;
+
+        root1->left = helper(root1->left, root2->left);
+        root1->right = helper(root1->right, root2->right);
+
+        return root1;
+    }
+};
